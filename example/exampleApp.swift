bc26758f22f1a4d9c8d83c2b901dfd3468ef7191
yhj0901 <yangheejune0901@gmail.com>
2023-02-03 11:56:34 +0900

    Initial Commit

---
+++ example/exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleApp.swift
+//  example
+//
+//  Created by yanghj on 2023/02/03.
+//
+
+import SwiftUI
+
+@main
+struct exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
