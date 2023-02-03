85437e6e1522c3d56172d488f0532c31f26e352f
yhj0901 <yangheejune0901@gmail.com>
2023-02-03 17:36:21 +0900

    creating and Combining Views

--- example/ContentView.swift
+++ example/ContentView.swift
@@ -7,18 +7,43 @@
 
 import SwiftUI
 
+// 레이아웃 설명
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            MapView().frame(height:300)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock").font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
+// 미리보기
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
