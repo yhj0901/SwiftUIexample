1a48a0d4f5ae1bd9235539a0f9aa9154f8d94a82
yhj0901 <yangheejune0901@gmail.com>
2023-02-06 21:33:07 +0900

    추가 못한것 추가

---
+++ example/Views/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  example
+//
+//  Created by yanghj on 2023/02/03.
+//
+
+import SwiftUI
+
+// 레이아웃 설명
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+// 미리보기
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
